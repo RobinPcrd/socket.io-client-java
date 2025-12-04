227c88a5f16a9ff64e69d07005d2ea00338e36e1
RobinPcrd <robin.picard.dev@gmail.com>
2025-12-04 17:13:05 +0100

    fix

--- src/test/java/io/socket/client/Connection.java
+++ src/test/java/io/socket/client/Connection.java
@@ -54,7 +54,7 @@ public abstract class Connection {
         return Runtime.getRuntime().exec(String.format(script, nsp()), createEnv(port));
     }
 
-    private Future<?> startServerOutput(Process process, String serverName, CountDownLatch latch) {
+    private Future<?> startServerOutput(final Process process, String serverName, CountDownLatch latch) {
         return serverService.submit(new Runnable() {
             @Override
             public void run() {
@@ -74,7 +74,7 @@ public abstract class Connection {
         });
     }
 
-    private Future<?> startServerError(Process process, String serverName) {
+    private Future<?> startServerError(final Process process, String serverName) {
         return serverService.submit(new Runnable() {
             @Override
             public void run() {
